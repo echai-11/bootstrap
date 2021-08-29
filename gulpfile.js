71769485fe55088770960b8be1d4131214c722fc
Lizzie Chai <lizzie.chai@gmail.com>
2021-08-29 16:00:57 -0500

    gulp struggles part 2

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,15 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("node-sass"));
 const browserSync = require("browser-sync").create();
+const imagemin = require("gulp-imagemin");
+const del = require("del"),
+  uglify = require("gulp-uglify"),
+  usemin = require("gulp-usemin"),
+  rev = require("gulp-rev"),
+  cleanCss = require("gulp-clean-css"),
+  flatmap = require("gulp-flatmap"),
+  htmlmin = require("gulp-htmlmin");
+
 //compile scss into css
 function style() {
   return gulp
@@ -23,5 +32,55 @@ function watch() {
   gulp.watch("./js/*.js").on("change", browserSync.reload);
 }
 
+//clean
+
+function clean() {
+  return del(["dist"]);
+}
+function copyFonts() {
+  return gulp
+    .src("./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*")
+    .pipe(gulp.dest("dist/fonts"));
+}
+function minimizeImages() {
+  return gulp
+    .src("img/*.{png,jpg,gif}")
+    .pipe(
+      imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })
+    )
+    .pipe(gulp.dest("dist/img"));
+}
+function minify() {
+  return gulp
+    .src("./*.html")
+    .pipe(
+      flatmap(function (stream, file) {
+        return stream.pipe(
+          usemin({
+            css: [rev()],
+            html: [
+              function () {
+                return htmlmin({ collapseWhitespace: true });
+              },
+            ],
+            js: [uglify(), rev()],
+            inlinejs: [uglify()],
+            inlinecss: [cleanCss(), "concat"],
+          })
+        );
+      })
+    )
+    .pipe(gulp.dest("dist/"));
+}
+
 exports.style = style;
 exports.default = watch;
+exports.build = gulp.series(
+  clean,
+  copyFonts,
+  minimizeImages,
+  minify,
+  (done) => {
+    done();
+  }
+);
